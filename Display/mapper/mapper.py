5268fa0baaa5dffeb8286600eff56bec7717ebff
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-05-21 19:27:14 -0400

    rotation function implemented, some distortion

--- Display/mapper/mapper.py
+++ Display/mapper/mapper.py
@@ -7,8 +7,11 @@ import cv2, pygame
 green = (0,255,0)
 blue = (0,0,255)
 black = (0,0,0)
+white = (255,255,255)
 
 def changeRes(file_name,mode,width,height,res_x,res_y):
+
+    assert(0<=-width/height+res_x/res_y<=0.1)
     
     mode = mode.lower()
     
@@ -39,7 +42,7 @@ def changeRes(file_name,mode,width,height,res_x,res_y):
                     pixelated_img[int(i/scale_y)].append(1)
                 else:
                     pixelated_img[int(i/scale_y)].append(0)
-
+        
     return pixelated_img
 
 def printimgArray(array):
@@ -53,12 +56,18 @@ def recordData(fname, img_arr):
     file.write('{')
     
     for i in range(len(img_arr)):
+        file.write('{')
         for j in range(len(img_arr[i])):
-            if i == len(img_arr)-1 and j == len(img_arr[i])-1:
+            if j == len(img_arr[i])-1:
                 file.write(str(img_arr[i][j]))
             else:
                 file.write(str(img_arr[i][j])+',')
-        file.write('\n')
+
+        if i == len(img_arr)-1:
+            file.write('}')
+        else:
+            file.write('},\n')
+            
     file.write('}')
     file.close()
 
@@ -92,12 +101,90 @@ def drawMonocolor(width,height,col0,col1,array):
     pygame.time.delay(5000)
     pygame.quit()
 
+def rotatePlane(plane,angle,direction):
+    direction = direction.lower()
+    
+    horiz = True
+    skip = 0
+    countOn = False
+    direct = 1
+    
+    x = 0
+    y = 0
+    
+    
+    if direction == 'ccw':
+        horiz = False
+    
+    for rev in range(angle%45+1):
+        for diag in range(len(plane)-2,int(len(plane)/2),-1):
+
+            perimeter = 2**diag
+            x = 0
+            y = 0
+            
+            for i in range(perimeter):
+                
+                if not i%diag and i != 0:
+                    horiz = horiz ^ 1
+                    
+                if x == diag and y == diag:
+                    direct = -1
+                    
+                if direction == 'cw':
+                    if horiz:
+                        if plane[x][y] and skip == (diag-2):
+                            plane[x][y] = 0
+                            plane[x+(diag-2)*direct][y] = 1
+                        x += direct
+                        if countOn:
+                            skip += 1
+
+                            
+                    else:
+                        if plane[x][y] and skip == (diag-2):
+                            plane[x][y] = 0
+                            plane[x][y+(diag-2)*direct] = 1
+                        y += direct
+                        if countOn:
+                            skip += 1
+                        
+                elif direction == 'ccw':
+                    if horiz:
+                        if plane[x][y] and skip == (diag-2):
+                            plane[x][y] = 0
+                            plane[x-(diag-2)*direct][y] = 1
+                            
+                        x -= direct
+                        if countOn:
+                            skip += 1
+                    else:
+                        if plane[x][y] and skip == (diag-2):
+                            plane[x][y] = 0
+                            plane[x][y-(diag-2)*direct] = 1
+                        y -= direct
+                        if countOn:
+                            skip += 1
+            
+    return plane;
+            
+        
 ##Main Program
     
 def main():
     
     arr = changeRes('worldmap.png','grayscale',1024,512,64,32)
-    drawMonocolor(1024,512,blue,green,arr)
+    #   drawMonocolor(1024,512,blue,green,arr)
+    arr2 = changeRes('plane.jpg','grayscale',615,512,5,4)
+    arr2.append([0,0,0,0,0])
+    arr3 = rotatePlane(arr2,45,'cw')
+    printimgArray(arr3)
+    
+ 
+ #printimgArray(plane)
+ #recordData('plane.txt',plane)
+ #recordData('map.txt',arr)
+ #drawMonocolor(512,512,white,black,plane)
 
 
 main()
