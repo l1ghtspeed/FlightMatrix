4c15a6239ed7708766c3165c909c51daa808d87a
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-05-21 12:16:17 -0400

    generalized all functions, blockchain

--- Display/world-mapper/mapper.py
+++ Display/world-mapper/mapper.py
@@ -1,48 +1,52 @@
+#Program uses openCV to change the resolution of images and write their data to a text file
+
 import numpy as np
 import cv2, pygame
 
 #Colors
 green = (0,255,0)
 blue = (0,0,255)
+black = (0,0,0)
 
-#Screen Parameters
-SCREEN_WIDTH,SCREEN_HEIGHT = 1024,512
-pygame.init()
-##screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-##screen.fill((0,0,0))
-
-#pygame.display.update()
-
-#Load Image
-img = cv2.imread('worldmap.png',cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img,(1024,512))
-pixelated_img = [[] for i in range(32)]
-
-for i in range(0,497,16):
-    for j in range(0,1009,16):
-
-        num_black = 0
-        num_white = 0
+def changeRes(file_name,mode,width,height,res_x,res_y):
+    
+    mode = mode.lower()
+    
+    if mode == 'grayscale':
         
-        for k in range(16):
-            for l in range(16):
-                if img[i+k][j+l]>= 127:
-                    num_white+=1
+        img = cv2.imread(file_name,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img,(width,height))
+        
+        pixelated_img = [[] for i in range(res_y)]
+
+        scale_x = int(width/res_x)
+        scale_y = int(height/res_y)
+        
+        for i in range(0,height,scale_y):
+            for j in range(0,width,scale_x):
+
+                num_black = 0
+                num_white = 0
+                
+                for k in range(scale_y):
+                    for l in range(scale_x):
+                        if img[i+k][j+l]>= 127:
+                            num_white+=1
+                        else:
+                            num_black+=1
+                                              
+                if num_black >= num_white:            
+                    pixelated_img[int(i/scale_y)].append(1)
                 else:
-                    num_black+=1
-                    
-        #print("---",int(i/16),"     ",int(j/16),"---")                   
-        if num_black >= num_white:            
-            #pixelated_img[int(i/16)][int(j/16)] = green
-            pixelated_img[int(i/16)].append(1)
-        else:
-            #pixelated_img[int(i/16)][int(j/16)] = blue
-            pixelated_img[int(i/16)].append(0)
-            
-##for i in range (32):
-##    for j in range (64):
-##        print(pixelated_img[i][j],end='')
-##    print('')
+                    pixelated_img[int(i/scale_y)].append(0)
+
+    return pixelated_img
+
+def printimgArray(array):
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            print(array[i][j],end='')
+        print('')
 
 def recordData(fname, img_arr):
     file = open(fname,'w')
@@ -58,24 +62,42 @@ def recordData(fname, img_arr):
     file.write('}')
     file.close()
 
-recordData('map_array.txt',pixelated_img)
-#print(count/4096*100,"% is land")
-##while True:
-##    for i in range(32):
-##        for j in range(64):
-##            for event in pygame.event.get():
-##                if event.type == pygame.QUIT:
-##                    pygame.quit()
-##                    quit()
-##                    
-##                if pixelated_img[i][j]:
-##                    pygame.draw.rect(screen,green, (j*16,i*16,16,16))
-##                else:
-##                    pygame.draw.rect(screen,blue, (j*16,i*16,16,16))
-##
-##  #              pygame.draw.rect(screen,(255,255,255), (randint(0,512),randint(0,1024),16,16))
-##                pygame.display.update()
-##
-pygame.time.delay(5000)
-pygame.quit()
-quit()
+def drawMonocolor(width,height,col0,col1,array):
+    assert(len(col0) == 3 and len(col1) == 3)
+    
+    pygame.init()
+
+    screen = pygame.display.set_mode((width, height))
+    screen.fill(black)
+
+    scale_x = int(width/len(array[0]))
+    scale_y = int(height/len(array))
+
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    
+            if array[i][j]:
+                pygame.draw.rect(screen,col1, (j*scale_x,i*scale_y,16,16))
+            else:
+                pygame.draw.rect(screen,col0, (j*scale_x,i*scale_y,16,16))
+                    
+            pygame.display.update()
+            
+    pygame.display.update()
+    pygame.time.delay(5000)
+    pygame.quit()
+
+##Main Program
+    
+def main():
+    
+    arr = changeRes('worldmap.png','grayscale',1024,512,64,32)
+    drawMonocolor(1024,512,blue,green,arr)
+
+
+main()
