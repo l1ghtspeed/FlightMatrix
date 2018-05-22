753279956b16873eb3abcdf531de4a54675cea18
Ayush Kapur <ayushkapur10@gmail.com>
2018-05-22 18:58:21 -0400

    Worked on interrupt handler, added map to header file

---
+++ src/latLongConv.cpp
@@ -0,0 +1,3 @@
+#include "videoMash.hpp"
+#include <cmath>
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,30 @@
 #include "latlongConv.hpp"
+#include "led-matrix.h"
+#include <stdio.h>
+#include <signal.h>
+
+using rgb_matrix::GPIO;
+using rgb_matrix::RGBMatrix;
+using rgb_matrix::Canvas;
+
+//interrupt and interrupt handling setup
+volatile bool interrupt_received = flase;
+static void InterruptHandler(int signo) {
+	interrupt_received = true;
+}
+
+static void drawMap(Canvas *canvas) {
+
+}
+
+int main(int argc, char *argv[]) {
+	RGBMatrix::Options defaults;
+	defaults.harware_mapping = "adafruit-hat";
+	defaults.rows = 32;
+	defaults.columns = 64;
+	defaults.chain_length = 1;
+	defaults.parallel = 1;
+	defaults.show_refresh_rate_rate = true;
+	Canvas *canvas = rgb_matrix::CreateMatrixFromFlags(&argc, &argv, &defaults);
+};
+
