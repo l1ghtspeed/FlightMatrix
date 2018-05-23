2c8c2c6be99a5cdd684e8ec6ce3c168bf8b930db
Ayush Kapur <ayushkapur10@gmail.com>
2018-05-22 20:07:12 -0400

    Added latitude longitude parsing function, updated definitions

--- src/latLongConv.cpp
+++ src/latLongConv.cpp
@@ -1,7 +1,10 @@
 #include "videoMash.hpp"
 #include <cmath>
+#include <ifstream>
+#include <iostream>
+#include <vector>
 
-matCoord covertCoord(geoCoord latLong) {
+vector<matCoord> covertCoord(vector <geoCoord> latLong) {
 	//latitude, longitude
 	matCoord out;
 	int mapWidth = 64;
@@ -11,4 +14,15 @@ matCoord covertCoord(geoCoord latLong) {
 	float temp = log(tan((PI/4)+(latRad/2)));
 	out.y = int((mapHeight/2)-(mapWidth*temp/(2*PI)));
 	return out;
+}
+
+vector<geoCoord> getCoords() {
+	ifstream in;
+
+	in.open("out.txt");
+	string component;
+	while (in << component) {
+
+	}
+	in.close();
 }
