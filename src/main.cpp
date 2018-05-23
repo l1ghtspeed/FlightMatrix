4c83a264f3c231b20d0d3c7d05e6df7faa7f9523
Xelynega <noahmetz1@gmail.com>
2018-05-23 14:21:11 +0000

    Moss

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,11 @@
 #include <signal.h>
 #include <vector>
 #include <time.h>
+#include "graphics.h"
+#include <cassert>
+#include <unistd.h>
 
+using namespace std;
 using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
@@ -43,30 +47,31 @@ static void drawTime(Canvas* canvas){
 	RGBMatrix::Options matrix_options;
 	rgb_matrix::RuntimeOptions runtime_opt;
 
-	const char *bdf_font_file = "../fonts/8x13.bdf";
-	font.LoadFont(bdf_font_file);
-
-	Color color(255, 255, 0);
-
-	char x_orig = 0;
-	char y_orig = 0;
-	char letter_spacing = 0;
-
 	rgb_matrix::Font font;
 
-	const char x = x_orig;
+	const char *bdf_font_file = "../fonts/4x6.bdf";
+	font.LoadFont(bdf_font_file);
+	rgb_matrix::Color bg_color(0, 0, 255);
+	rgb_matrix::Color color(255, 255, 255);
+
+	char x_orig = 1;
+	char y_orig = 26;
+	char letter_spacing = 0;
+
+	char x = x_orig;
 	char y = y_orig;
 
-	char time_arr[6];
+	char time_arr[5];
+	
 	time_t t = time(NULL);
-	struct tm tm = *localtime(&t);
+	struct tm tme = *localtime(&t);
 
-	time_arr[0] = (char) tm.tm_hour/10;
-	time_arr[1] = (char) tm.tm_hour%10;
+	time_arr[0] = (char) ((tme.tm_hour-4 < 0)?24-(tme.tm_hour-4):tme.tm_hour-4)/10 + 48;
+	time_arr[1] = (char) ((tme.tm_hour-4 < 0)?24-(tme.tm_hour-4):tme.tm_hour-4)%10 + 48;
 	time_arr[2] = ':';
-	time_arr[3] = (char) tm.tm_min/10;
-	time_arr[4] = (char) tm.tm_min%10;
-	time_arr[5] = '/0';
+	time_arr[3] = (char) tme.tm_min/10 + 48;
+	time_arr[4] = (char) tme.tm_min%10 + 48;
+	//time_arr[5] = '/0';
 
 	// The outline font, we need to write with a negative (-2) text-spacing,
 	// as we want to have the same letter pitch as the regular text that
@@ -77,10 +82,10 @@ static void drawTime(Canvas* canvas){
 
 	// The regular text. Unless we already have filled the background with
 	// the outline font, we also fill the background here.
-	rgb_matrix::DrawText(canvas, font, x, y + font.baseline(),
-						color, line,
-						letter_spacing);
+	rgb_matrix::DrawText(canvas, font, x, y + font.baseline(),color, &bg_color ,time_arr,-1);
+
 	y += font.height();
+
 
 }
 
@@ -93,24 +98,26 @@ static void drawAirports(Canvas *canvas,vector<matCoord> &airports){
 
 static void addAirports(vector<matCoord> &airports){
 	matCoord mat;
-	mat.x = 47;mat.y = 9;airports.push_back(mat);
-	mat.x = 52;mat.y = 8;airports.push_back(mat);
-	mat.x = 32;mat.y = 5;airports.push_back(mat);
-	mat.x = 47;mat.y = 7;airports.push_back(mat);
-	mat.x = 56;mat.y = 9;airports.push_back(mat);
-	mat.x = 53;mat.y = 9;airports.push_back(mat);
-	mat.x = 32;mat.y = 5;airports.push_back(mat);
-	mat.x = 49;mat.y = 9;airports.push_back(mat);
-	mat.x = 33;mat.y = 5;airports.push_back(mat);
-	mat.x = 52;mat.y = 11;airports.push_back(mat);
+	mat.x = 8;mat.y = 13;airports.push_back(mat);
+	mat.x = 14;mat.y = 11;airports.push_back(mat);
+	mat.x = 15;mat.y = 13;airports.push_back(mat);
+	mat.x = 18;mat.y = 11;airports.push_back(mat);
+	mat.x = 58;mat.y = 13;airports.push_back(mat);
+	mat.x = 12;mat.y = 13;airports.push_back(mat);
+	mat.x = 34;mat.y = 9;airports.push_back(mat);
+	mat.x = 16;mat.y = 13;airports.push_back(mat);
+	mat.x = 35;mat.y = 9;airports.push_back(mat);
+	mat.x = 54;mat.y = 15;airports.push_back(mat);
 }
 
 int main(int argc, char *argv[]) {
 	RGBMatrix::Options defaults;
+	vector<matCoord> airports, planes;
+	addAirports(airports);
 	defaults.hardware_mapping = "adafruit-hat";
 	defaults.rows = 32;
 	defaults.cols = 64;
-	defaults.brightness = 50;
+	defaults.brightness = 25;
 	defaults.chain_length = 1;
 	defaults.parallel = 1;
 	defaults.brightness=50;
@@ -123,9 +130,13 @@ int main(int argc, char *argv[]) {
 	signal(SIGTERM, InterruptHandler);
 	signal(SIGINT, InterruptHandler);
 
-	while(!interrupt_recieved){
+	while(!interrupt_received){
 		drawMap(canvas);
-		drawAirports(canvas);
+		drawTime(canvas);
+		drawAirports(canvas,airports);
+		getPlaneCoord(planes);
+		drawPlanes(canvas, planes);
+		usleep(10000000);
 	}
 	
 	//clear and delete the canvas if interrupt is sent
