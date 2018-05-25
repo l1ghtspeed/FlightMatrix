0c9eef9d019a7347adf310056bd676f9b356d9f0
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-05-24 22:47:36 -0400

    main reads ip address

--- src/moss.cpp
+++ src/moss.cpp
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <iostream>
+#include "stdlib.h"
+#include <string>
 #include <fstream>
+#include <cassert>
 
-int main(){
+using namespace std;
+string getIP(string file_name){
+    ifstream f;
+    f.open(file_name);
+    string msg;
+    short read = 5;
+    string ipAddress = "";
+
+    getline(f,msg);
+    assert(msg.length());
     
+    while((char) msg.at(read) != 32){
+        if((char) msg.at(read) != 46){
+            ipAddress += msg.at(read);
+        }
+        read++;
+    }
+
+    return ipAddress;
+}
+
+int main(int argc, char* argv[]){
+    string x = getIP("ip.txt");
+    cout << x << endl;
     return 0;
 }
