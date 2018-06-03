49468eec1f1a85a86c086a8c6175a5958c2a2477
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-06-03 19:19:19 -0400

    email generator implemented

--- backend/getApiKey.py
+++ backend/getApiKey.py
@@ -1,38 +1,49 @@
-import mechanicalsoup
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 import random
+import requests
 
-browser = mechanicalsoup.StatefulBrowser()
+s = requests.Session()
+
+def fetch(url, data=None):
+    if data is None:
+        return s.get(url).content
+    else:
+        return s.post(url, data=data).content
+
 
 def getName():
-  firstname = ""
-  lastname = ""
+  flist = open("firstnames.txt",'r')
+  llsit = open("lastnames.txt",'r')
+  firstname = flist.readline(random.randint(0,6296))
+  lastname = llist.readline(random.randint(0,4945))
   return firstname,lastname
+
 def getUserName(first,last):
   username = first + last
   number_cnt = random.randint(0, 30 - len(username))
   for i in range(number_cnt):
     username += str(random.randint(0,9))
-username = firstname + lastname
+
+def getEmail():
+  req = Request('https://maildrop.cc/', headers={'User-Agent': 'Chrome'})
+  page = urlopen(req)
+  soup = BeautifulSoup(page, 'html.parser')
+  email_box = soup.find('div', attrs={'id': 'suggestion'})
+  return email_box.text.strip()
+
+def verifyEmail(email):
+  req = Request('https://maildrop.cc/'+'/inbox'+email[:12], headers={'User-Agent': 'Chrome'})
+  page = urlopen(req)
+  soup = BeautifulSoup(page, 'html.parser')
+  #form = soup.find('form')
+  #fields = form.findAll('input')
+  #formdata = dict( (field.get('name'), field.get('value')) for field in fields)
+  #formdata['mailbox'] = email
+  print ("s")
 
 
 
-browser.open("https://flightaware.com/commercial/flightxml/signup.rvt?product_type=1")
-browser.follow_link("/account/join/")
-
-browser.select_form('form[method="post"]')
-browser["name_first"] = "Michael Oxlong"
-#browser["custtel"] = "00 00 0001"
-#browser["custemail"] = "nobody@example.com"
-#browser["size"] = "medium"
-#browser["topping"] = "onion"
-#browser["topping"] = ("bacon", "cheese")
-#browser["comments"] = "This pizza looks really good :-)"
-print(browser.get_current_page())
-# Uncomment to launch a real web browser on the current page.
-browser.launch_browser()
-
-# Uncomment to display a summary of the filled-in form
-# browser.get_current_form().print_summary()
-
-#response = browser.submit_selected()
-#print(response.text)
+print(getEmail())
+verifyEmail(getEmail())
+#page = urllib2.urlopen("https://flightaware.com/commercial/flightxml/signup.rvt?product_type=1")
